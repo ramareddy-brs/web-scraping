21a8998a24dfaeb37b8513a4a150c85d9a14cc9e
Ramareddy <ramareddy9652@gmail.com>
2024-04-18 11:00:26 +0530

    first commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,6 @@
+// Change Theme
+function setTheme(theme) {
+  document.documentElement.style.setProperty("--primary-color", theme);
+  localStorage.setItem("movie-theme", theme);
+}
+setTheme(localStorage.getItem("movie-theme") || chathams_blue);
